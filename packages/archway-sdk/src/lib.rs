f4eab2dabc34db90a5311c5c7f03b7b06a30cb65
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-01-23 19:37:10 +0100

    Remove control archway chain

--- packages/archway-sdk/src/lib.rs
+++ packages/archway-sdk/src/lib.rs
@@ -5,5 +5,5 @@ pub mod types;
 
 // This is a signal, such that any contract that imports these helpers will only run on the
 // archway blockchain
-#[no_mangle]
-extern "C" fn requires_archway() {}
+// #[no_mangle]
+// extern "C" fn requires_archway() {}
