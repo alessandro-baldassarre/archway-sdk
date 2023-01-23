6e3e97bc09bbfcfa5ee4ba010ea53323dc98b9e4
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-01-23 18:57:26 +0100

    Update custom query

--- packages/archway-sdk/src/custom/query.rs
+++ packages/archway-sdk/src/custom/query.rs
@@ -1,69 +1,40 @@
+use cosmwasm_schema::cw_serde;
 use cosmwasm_std::CustomQuery;
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
 
-use super::{Coins, PageRequest, PageResponse};
+use crate::types::{
+    archwayrewardsv1beta1::{QueryContractMetadataRequest, QueryRewardsRecordsRequest},
+    cosmosbasequeryv1beta1::PageRequest,
+};
 
-#[non_exhaustive]
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
+#[cw_serde]
 pub enum ArchwayQuery {
-    ContractMetadata {
-        contract_address: String,
-    },
-    RewardsRecords {
-        rewards_address: String,
-        pagination: Option<PageRequest>,
-    },
+    ContractMetadata(QueryContractMetadataRequest),
+    RewardsRecords(QueryRewardsRecordsRequest),
 }
 
 impl CustomQuery for ArchwayQuery {}
 
 impl ArchwayQuery {
     pub fn contract_metadata(contract_address: impl Into<String>) -> Self {
-        ArchwayQuery::ContractMetadata {
+        ArchwayQuery::ContractMetadata(QueryContractMetadataRequest {
             contract_address: contract_address.into(),
-        }
+        })
     }
 
     pub fn rewards_records(rewards_address: impl Into<String>) -> Self {
-        ArchwayQuery::RewardsRecords {
+        ArchwayQuery::RewardsRecords(QueryRewardsRecordsRequest {
             rewards_address: rewards_address.into(),
             pagination: None,
-        }
+        })
     }
 
     pub fn rewards_records_with_pagination(
         rewards_address: impl Into<String>,
         pagination: PageRequest,
     ) -> Self {
-        ArchwayQuery::RewardsRecords {
+        ArchwayQuery::RewardsRecords(QueryRewardsRecordsRequest {
             rewards_address: rewards_address.into(),
             pagination: Some(pagination),
-        }
+        })
     }
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub struct ContractMetadataResponse {
-    pub owner_address: String,
-    pub rewards_address: String,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub struct RewardsRecordsResponse {
-    pub records: Vec<RewardsRecord>,
-    pub pagination: Option<PageResponse>,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub struct RewardsRecord {
-    pub id: u64,
-    pub rewards_address: String,
-    pub rewards: Coins,
-    pub calculated_height: i64,
-    pub calculated_time: String,
 }
