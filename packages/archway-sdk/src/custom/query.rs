f003eeb934936a74f27a59bfafe27d9de2a39d66
Alessandro Baldassarre <alessandro.baldassarre.dev@gmail.com>
2023-01-23 20:08:41 +0100

    Update custom query

--- packages/archway-sdk/src/custom/query.rs
+++ packages/archway-sdk/src/custom/query.rs
@@ -1,40 +1,42 @@
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::CustomQuery;
 
-use crate::types::{
-    archwayrewardsv1beta1::{QueryContractMetadataRequest, QueryRewardsRecordsRequest},
-    cosmosbasequeryv1beta1::PageRequest,
-};
+use crate::types::cosmosbasequeryv1beta1::PageRequest;
 
 #[cw_serde]
 pub enum ArchwayQuery {
-    ContractMetadata(QueryContractMetadataRequest),
-    RewardsRecords(QueryRewardsRecordsRequest),
+    ContractMetadata {
+        contract_address: String,
+    },
+    RewardsRecords {
+        rewards_address: String,
+        pagination: Option<PageRequest>,
+    },
 }
 
 impl CustomQuery for ArchwayQuery {}
 
 impl ArchwayQuery {
     pub fn contract_metadata(contract_address: impl Into<String>) -> Self {
-        ArchwayQuery::ContractMetadata(QueryContractMetadataRequest {
+        ArchwayQuery::ContractMetadata {
             contract_address: contract_address.into(),
-        })
+        }
     }
 
     pub fn rewards_records(rewards_address: impl Into<String>) -> Self {
-        ArchwayQuery::RewardsRecords(QueryRewardsRecordsRequest {
+        ArchwayQuery::RewardsRecords {
             rewards_address: rewards_address.into(),
             pagination: None,
-        })
+        }
     }
 
     pub fn rewards_records_with_pagination(
         rewards_address: impl Into<String>,
         pagination: PageRequest,
     ) -> Self {
-        ArchwayQuery::RewardsRecords(QueryRewardsRecordsRequest {
+        ArchwayQuery::RewardsRecords {
             rewards_address: rewards_address.into(),
             pagination: Some(pagination),
-        })
+        }
     }
 }
